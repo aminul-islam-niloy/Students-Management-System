ae4088136635ace555b23fcd4bb13cd1b2f8ecc3
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-15 01:23:14 +0600

    Student Management Guna and .Net Framework

--- Students Management/Program.cs
+++ Students Management/Program.cs
@@ -9,9 +9,7 @@ namespace Students_Management
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
